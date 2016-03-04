b49dc74bf6300ceda3f612982be80da0eb78e99a
Luciano Saraiva Dias <lucianod@dbserver.com.br>
2016-03-04 17:45:41 -0300

    controllers convertidos

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,22 +1,40 @@
 /// <reference path="../typings/main.d.ts" />
+class DashCtrl {
+    constructor() { }
+}
+
+class ChatsCtrl {
+    public $inject = ['Chats'];
+    chats: any[];
+    constructor(public Chats: any) {
+        this.chats = Chats.all();
+    }
+    remove(chat) {
+        this.Chats.remove(chat);
+    }
+}
+
+class ChatDetailCtrl {
+    public $inject = ['Chats', '$stateParams'];
+    chat: Object;
+    constructor(
+        public Chats: any,
+        public $stateParams: ng.ui.IStateParamsService) {
+        this.chat = Chats.get($stateParams.chatId);
+    }
+}
+
+class AccountCtrl {
+    settings: Object;
+    constructor() {
+        this.settings = {
+            enableFriends: true
+        };
+    }
+}
+
 angular.module('starter.controllers', [])
-
-  .controller('DashCtrl', function() {
-})
-
-  .controller('ChatsCtrl', function(Chats) {
-  this.chats = Chats.all();
-  this.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-  .controller('ChatDetailCtrl', function($stateParams, Chats) {
-  this.chat = Chats.get($stateParams.chatId);
-})
-
-  .controller('AccountCtrl', function() {
-  this.settings = {
-    enableFriends: true
-  };
-});
+    .controller('DashCtrl', DashCtrl)
+    .controller('ChatsCtrl', ChatsCtrl)
+    .controller('ChatDetailCtrl', ChatDetailCtrl)
+    .controller('AccountCtrl', AccountCtrl);
